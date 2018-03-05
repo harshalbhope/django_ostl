7aba7ca7555b2f8146be57a6c66ff154c7998056
HARSHALB <harshalbhope67@gmail.com>
2018-03-06 02:08:15 +0530

    added database

--- music/models.py
+++ music/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class Book(models.Model):
+    Author = models.CharField(max_length=250)
+    Publisher = models.CharField(max_length=250)
+    Type = models.CharField(max_length=100)
+    Book_logo = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.Publisher+'-'+ self.Author
 # Create your models here.
+class Book_name(models.Model):
+    Book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    Book_year = models.CharField(max_length=100)
+    file_type = models.CharField(max_length=250)
--- music/views.py
+++ music/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("<h1>thos ososo</h1>")
+
+def detail(request,Book_id):
+    return HttpResponse("<h2>details for book_id:"+ str(Book_id)+"</h2>")

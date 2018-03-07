43b47b8fddb603265964fbda6fae779ed1239f5a
HARSHALB <harshalbhope67@gmail.com>
2018-03-08 00:34:49 +0530

    connecting database

--- music/views.py
+++ music/views.py
@@ -1,16 +1,11 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Book
-from django.shortcuts import render
-
 
 def index(request):
     all_books = Book.objects.all()
-
-    html = ''
-    for books in all_books:
-        path = '/music/'+str(books.id)+'/'
-        html += '<a href ="'+path+'">'+books.Publisher+'</a><br>'
-    return HttpResponse(html)
+    template = loader.get_template('')
+    return HttpResponse('')
 
 def detail(request, book_id):
     return HttpResponse("<h2>details for book_id:" + str(book_id) + "</h2>")
